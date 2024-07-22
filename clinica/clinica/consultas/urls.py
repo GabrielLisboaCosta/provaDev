f84bdea6290e4d2e499d2f59d3034d3b2bc75caa
Gabriel <gabrielcosta1055@hotmail.com>
2024-07-22 20:53:20 -0300

    att

--- clinica/clinica/consultas/urls.py
+++ clinica/clinica/consultas/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('saudacao', views.saudacao),
     path('saudacao/<str:nome>', views.parametro),
     path('desafio/<str:nome>', views.desafio),
-    path('paciente/<int:id>', views.paciente),
-    path('paciente', views.list_pacientes),
-    path('atendimento/<int:id>', views.atendimento)
+    path('paciente/read/<int:id>', views.paciente, name="read_paciente"),
+    path('paciente', views.list_pacientes, name="pacientes"),
+    path('atendimento/<int:id>', views.atendimento),
+    # path('paciente/update/<int:paciente_id>', views.update, name="update_pacientes"),
+    path('paciente/delete/<int:paciente_id>', views.delete, name="delete_pacientes"),
 ]
--- clinica/clinica/consultas/views.py
+++ clinica/clinica/consultas/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from consultas.models import Paciente, Atendimento
@@ -61,3 +61,22 @@ def atendimento(request, id):
     }
 
     return render(request, "consultas/atendimento.html", contexto)
+
+
+def delete(request, paciente_id):
+    paci = get_object_or_404(Paciente, pk=paciente_id)
+    try:
+        if request.method == 'POST':
+            v_paciente_id = request.POST.get("paciente_id", None)
+            if int(v_paciente_id) == paciente_id:
+                paci.delete()
+                return redirect('consultas:pacientes')
+        else:
+            contexto = {
+                "paciente": paci,
+            }
+            return render(request, "consultas/delete_paciente.html", contexto)
+    except Exception as e:
+        contexto = {}
+        print(e)
+        return render(request, "consultas/pacientes.html", contexto)
